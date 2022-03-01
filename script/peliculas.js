61bf9bd0d5a4778127356b2334335520b1f70305
David Puerto Guerrero <dsp5502@gmail.com>
2022-02-28 22:09:56 -0500

    Agregar informacion de repaso JS

---
+++ script/peliculas.js
@@ -0,0 +1,45 @@
+console.warn('Delcarar variables');
+let nombrePlanetas = ['Tierra', 'marte'],
+  distancia = [5, 3, '67'];
+
+const tamaño = [];
+
+console.log({ nombrePlanetas });
+console.log({ distancia });
+console.log({ tamaño });
+
+console.warn('Ciclo while');
+let i = 0;
+while (i <= nombrePlanetas.length - 1) {
+  console.log({ i });
+  console.log(nombrePlanetas[i]);
+  i += 1;
+}
+
+console.warn('Ciclo for');
+for (let i = 0; i < nombrePlanetas.length; i++) {
+  console.log(nombrePlanetas[i]);
+}
+
+console.warn('Ciclo for In');
+for (const key in nombrePlanetas) {
+  console.log(nombrePlanetas[key]);
+}
+console.warn('Ciclo for of');
+for (const planeta of nombrePlanetas) {
+  console.log(planeta);
+}
+
+console.warn('Ciclo for in Distancias ==');
+for (const key in distancia) {
+  distancia[key] == '5'
+    ? console.log('Se encontró la Distancia')
+    : console.log('No se encontró la distancia');
+}
+
+console.warn('Ciclo for in Distancias ===');
+for (const key in distancia) {
+  distancia[key] === '5'
+    ? console.log('Se encontró la Distancia')
+    : console.log('No se encontró la distancia');
+}
